3afeb56f4f5448c462d1f1459062a669eeb4f687
Sebastian Clancy <foldit.com@gmail.com>
2024-03-31 15:13:32 -0400

    Started custom run design.

---
+++ script/custom.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import math
+import pprint
+import random
+
+import numpy as np
+import torch
+from torch.optim import lr_scheduler
+
+from torchdrug import core, models, tasks, datasets, utils
+from torchdrug.utils import comm
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import util
+from gearnet import model, task, siamdiff
+from gearnet import model, cdconv, gvp, dataset, task, protbert
+
+if __name__ == "__main__":
+    args, vars = util.parse_args()
+    cfg = util.load_config(args.config, context=vars)
+    working_dir = util.create_working_directory(cfg)
+
+    # Set seed:
+    seed = args.seed
+    torch.manual_seed(seed + comm.get_rank())
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    dataset = core.Configurable.load_config_dict(cfg.dataset)
